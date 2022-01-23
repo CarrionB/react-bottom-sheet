cdae66b74babe1440a63d313052b83184c2dfa8a
CarrionB <ssj4_bryan@yahoo.es>
2022-01-23 12:18:21 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Sheet from 'react-modal-sheet';
+import { Button } from 'reactstrap';
+import './App.css'
 
-function App() {
+const  Example = () => {
+  const [isOpen, setOpen] = React.useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => setOpen(!isOpen)}>Abrir</button>
+
+      <Sheet
+        isOpen={isOpen}
+        onClose={() => setOpen(false)}
+        snapPoints={[600, 250, 100, 0]}
+        initialSnap={1}
+      >
+        <Sheet.Container>
+          <Sheet.Header/>
+          <Sheet.Content>
+            <div className='p-2'>
+              <div className='header-container'>
+                <img 
+                  className='logo' 
+                  src="https://impartialreader.com/icons/windows11/Square44x44Logo.scale-125.png" 
+                  alt="logo"/>
+                <div className='title-container'>
+                  Impartial Reader
+                  <div className='url'>
+                    impartialreader.com
+                  </div>
+                </div>
+                <div className='button-container'>
+                  <Button className='rounded-pill install-button' color='primary'>Install</Button>
+                </div>
+              </div>
+              <div className='gallery'>
+                <img 
+                  className='image' 
+                  src="https://impartialreader.com/screenshots/impartial1.png" 
+                  alt="screenshot1"/>
+                <img 
+                  className='image' 
+                  src="https://impartialreader.com/screenshots/impartial2.png" 
+                  alt="screenshot2"/>
+                <img 
+                  className='image' 
+                  src="https://impartialreader.com/screenshots/impartial3.png" 
+                  alt="screenshot3"/>
+                <img 
+                  className='image' 
+                  src="https://impartialreader.com/screenshots/impartial4.png" 
+                  alt="screenshot4"/>
+              </div>
+            </div>
+          </Sheet.Content>
+        </Sheet.Container>
+      </Sheet>
+    </>
   );
 }
 
-export default App;
+export default Example
